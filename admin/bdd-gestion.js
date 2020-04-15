931bbbb33a66ba039a0b460415b3571408939378
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-04-15 14:42:19 +0200

    new bdd

--- admin/bdd-gestion.js
+++ admin/bdd-gestion.js
@@ -11,11 +11,19 @@ let db = new sq.Database("./database.db", err => {
 
 // create();
 
-// for (let i = 1; i <= 12; i++){
+
+
+
+
+// for (let i = 1; i <= 5; i++){
 // 	insert(i);
 // }
 
+
+
 view();
+
+
 
 function create() {
 	db.run("CREATE TABLE article(id INTEGER PRIMARY KEY NOT NULL, title TEXT, category TEXT, content TEXT, date TEXT, img TEXT, visibility TEXT)");	
@@ -23,7 +31,7 @@ function create() {
 
 
 function insert(index) {
-	db.run("INSERT INTO article (title, category, content, date, img, visibility) VALUES(?,?,?,?,?,?)", ["title", "category", "content", "date", "img", "public"]);
+	db.run("INSERT INTO article (title, category, content, date, img, visibility) VALUES(?,?,?,?,?,?)", ["Mises à Jour : La guerre des dieux", "Mise à jour", "La nouvelle grosse mise à jour est sortie, retrouvez-vous au coeur de la guerre des dieux, un affrontement sans précédent entre les deux plus grandes puissances militaires de toute la galaxie. Cette mise à jour apporte un lot d'armes militaire qui pourront vous servir pour vous défendre contre les créatures hostiles et les chasseurs. Les équipes de plus de 100 personnes auront accès au plus grand vaisseau ayant jamais exister, le 'croiseur antique', qui est composé de 600 canons à Protons.", "25/06/2019", "../image/news/news1.png", "private"]);
 }
 
 
