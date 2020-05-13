41187075a771a04791a1673db1faf1de690c4413
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-05-13 09:37:26 +0200

    write ad path

--- admin/dist/write.dev.js
+++ admin/dist/write.dev.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
 Création d'article
 ------------------
@@ -8,39 +10,21 @@ Création d'article
 4- on crée un fetch avec un methode HTTP POST qui envoi les données sous forme d'un objet javascript
 
 */
-
-
-
-
-
 //écrire le code ici
-
-
-
-
-
 //exemple du fetch pour envoyer les données à l'API
 //ce qui est à l'intérieur du "body: JSON.stringify" est indicatif c'est pas forcément la même chose que pour univership
-fetch(`https://univership.herokuapp.com/create`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        poster: variable1,
-        title: variable2,
-        author: variable3,
-        content: varaible4
-    })
-})
-
-
-
-
-
-
-
-
-
-
+fetch("https://univership.herokuapp.com/create", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    poster: variable1,
+    title: variable2,
+    author: variable3,
+    content: varaible4
+  })
+});
 /*
 Edition d'article
 
@@ -52,10 +36,4 @@ Edition d'article
 
 
 */
-
-
-
-
-
-
 //écrire le code ici
---
+++ admin/dist/write.prod.js
@@ -0,0 +1,1 @@
+"use strict";fetch("https://univership.herokuapp.com/create",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({poster:variable1,title:variable2,author:variable3,content:varaible4})});
