b550891adf07f95ae212900923c51290277d5ff3
AurianeLab <34133072+AurianeLab@users.noreply.github.com>
2020-05-27 14:10:40 +0200

    Update faq.js

--- admin/faq.js
+++ admin/faq.js
@@ -8,3 +8,29 @@
 	5- On envoi les données grâce à un fetch vers https://univership.herokuapp.com/faq/create
 	6- on recharge la page pour afficher la liste complète des questions
 */
+
+function create() {
+    //écrire le code ici
+    //Réupération des infos de l'article
+    document.querySelector('button').addEventListener('click', () => {
+        let question = document.querySelector('#article-question').value;
+        let answer = document.querySelector('#article-answer').value;
+
+
+        //remise à zéro des inputs
+        document.querySelector('#article-question').value = "";
+        document.querySelector('#article-answer').value = "";
+
+
+        //envoi des informations du nouvelle article au serveur
+        fetch(`https://univership.herokuapp.com/faq/create`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                question: question,
+                answer: answer
+            })
+        })
+        .then(() => window.location.href = 'manage-faq.html');
+    });
+}
