3d5ebb6204328f4d5b64459489224f2dd8eb22ec
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-06-21 12:56:13 +0200

    valid token pass

--- admin/expire.js
+++ admin/expire.js
@@ -8,14 +8,10 @@ fetch(`https://univership.herokuapp.com/expired`, {
 .then(r => r.json())
 .then((data)=>{
     console.log(data);
-    if (data.status !== undefined) {
-        console.log(data.status);
-        if (data.status == 'token expired') { window.location.href = '../connexion.html?message=expired'; localStorage.removeItem('token');};
-        if (data.status == 'token not found') window.location.href = '../connexion.html?message=connect'
-        else window.location.href = '../connexion.html';
-    } else {
-        console.log('token valid');
-    }
+    console.log(data.status);
+    if (data.status == 'token expired') { window.location.href = '../connexion.html?message=expired'; localStorage.removeItem('token');};
+    if (data.status == 'token not found') window.location.href = '../connexion.html?message=connect'
+    else if (data.status !== 'valid') window.location.href = '../connexion.html';
 })
 
 
